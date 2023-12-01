0bb3bfc80534cc2dbf6222a7bb6fa2ebb944b8d8
Marshall Okafor <marshallokafor@Marshalls-MBP.attlocal.net>
2023-12-01 07:20:13 -0600

    added solution to LeetCode problem 36

---
+++ Arrays_hashing/validSudoku.py
@@ -0,0 +1,51 @@
+"""
+This is solution to LeetCode problem 36.
+
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according 
+to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+Runtime: O(9 ^ 2)
+"""
+
+import collections
+
+class Solution:
+    def isValidSudoku(self, board: list[list[str]]) -> bool:
+        
+        # Create hash set to store the values
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        squares = collections.defaultdict(set) # (r /3, c/3) for the sub boxes
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    continue
+                if (board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r//3, c//3)]):
+                    return False
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
+
+        return True
+    
+# Driver code
+if __name__ == '__main__':
+
+    sol = Solution()
+    
+    board = [["5","3",".",".","7",".",".",".","."]
+            ,["6",".",".","1","9","5",".",".","."]
+            ,[".","9","8",".",".",".",".","6","."]
+            ,["8",".",".",".","6",".",".",".","3"]
+            ,["4",".",".","8",".","3",".",".","1"]
+            ,["7",".",".",".","2",".",".",".","6"]
+            ,[".","6",".",".",".",".","2","8","."]
+            ,[".",".",".","4","1","9",".",".","5"]
+            ,[".",".",".",".","8",".",".","7","9"]]
+    
+    print(sol.isValidSudoku(board))
